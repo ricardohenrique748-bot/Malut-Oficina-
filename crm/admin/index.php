1df6b3ccc1373447045b5602bbaf6dc6b063abfb
Ricardo Henrique <ricardohenrique748@gmail.com>
2026-01-23 21:11:48 -0300

    Remove legacy folders to clean up build environment

--- crm/admin/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once '../config.php';
-if (is_logged_in()) {
-    header('Location: /admin/kanban.php');
-} else {
-    header('Location: /admin/login.php');
-}
-exit;
-?>
--- crm/admin/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once '../config.php';
-$_SESSION = array();
-session_destroy();
-header('Location: /admin/login.php');
-exit;
-?>
